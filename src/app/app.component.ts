8651ddb4484229ce256a94cc06898a92bbe23bf5
M.Michail <marriamichail@gmail.com>
2024-03-31 21:57:13 +0300

    all things to 06

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,20 @@ import { Component } from '@angular/core';
 import { PersonTableComponent } from './components/person-table/person-table.component';
 import { Person } from './shared/interfaces/person';
 import { EventBindExampleComponent } from './components/event-bind-example/event-bind-example.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 /*to @Component einai o decorator ths "export class" leitourgei san annotation /*gia na ta syndeei, an parembalei kati anamesa den tha leitourgei!
 */
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PersonTableComponent, EventBindExampleComponent],
+  imports: [PersonTableComponent, EventBindExampleComponent,
+  RouterLink,
+  RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  name = 'Maria';
 
   person0 = {
     givenName: 'Maria',
