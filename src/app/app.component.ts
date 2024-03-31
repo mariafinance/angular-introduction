c0a89474776ca5c4e16ffdc986ad426e4c18b442
M.Michail <marriamichail@gmail.com>
2024-03-31 17:21:40 +0300

    all changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,16 @@
 import { Component } from '@angular/core';
+import { PersonTableComponent } from './components/person-table/person-table.component';
 /*to @Component einai o decorator ths "export class" leitourgei san annotation /*gia na ta syndeei, an parembalei kati anamesa den tha leitourgei!
 */
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   name = 'Maria';
 
-  person = {
-    givenName: 'Maria',
-    surName: 'Michail',
-    age: 0x32,
-    email: 'test@aueb.gr',
-  };
+  
 }
