ca333958d20efe989314d8942c1622944a1d019f
M.Michail <marriamichail@gmail.com>
2024-04-01 20:03:56 +0300

    07

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,16 @@ import { PersonTableComponent } from './components/person-table/person-table.com
 import { Person } from './shared/interfaces/person';
 import { EventBindExampleComponent } from './components/event-bind-example/event-bind-example.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { ListGroupMenuComponent } from './components/list-group-menu/list-group-menu.component';
 /*to @Component einai o decorator ths "export class" leitourgei san annotation /*gia na ta syndeei, an parembalei kati anamesa den tha leitourgei!
 */
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PersonTableComponent, EventBindExampleComponent,
-  RouterLink,
-  RouterOutlet
+  imports: [
+    ListGroupMenuComponent,
+    RouterLink,
+    RouterOutlet
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
