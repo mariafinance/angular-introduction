d82721d8e8745e0cc368ad53ee68668d1c4fb4a3
M.Michail <marriamichail@gmail.com>
2024-04-23 18:13:32 +0300

    t

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -19,8 +19,8 @@ export class UserService {
 
 constructor () {
   const access_token = localStorage.getItem('access_token')
-  const decodedTokenSubject = jwtDecode(access_token).sub as unknown as LoggedInUser;
   if(access_token) {
+    const decodedTokenSubject = jwtDecode(access_token).sub as unknown as LoggedInUser;
     this.user.set({
       fullname :decodedTokenSubject.fullname,
       email: decodedTokenSubject.email,
